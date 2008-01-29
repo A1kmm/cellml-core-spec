028159ccaf4b7f24ebacdde242c4aeae329a21ff
Andrew Miller <ak.miller@auckland.ac.nz>
2008-01-30 11:53:38 +1300

    Updated the specification upload script to not hard-code my member name, and to name the upload location differently depending on the branch.

--- upspec.sh
+++ upspec.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 ./genspec.sh
-echo -e "put toplevel.xhtml\nquit" | cadaver http://www.cellml.org/Members/miller/draft-normative-spec/
+
+BRANCH=`git branch | grep "*" | cut -d" " -f2`
+
+if [[ $BRANCH == "normative" ]]; then
+    BRANCH=
+else
+    BRANCH="-$BRANCH"
+fi
+
+CELLML_MEMBER=`cat ~/.cellml_member`
+
+URL="http://www.cellml.org/Members/$CELLML_MEMBER/draft-normative-spec$BRANCH/"
+
+echo -e "put toplevel.xhtml\nquit" | cadaver $URL
